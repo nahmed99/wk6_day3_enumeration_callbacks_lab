447240af545284ee306648c9328940b7f358e550
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2020-12-09 14:52:11 +0000

    getFilmTitles coded and tested

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,15 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+
+Cinema.prototype.getFilmTitles = function() {
+  const result = this.films.map((film) => {
+    return film.title;    // This returns from the inner, callback function to the parent function
+  });
+
+  return result;    // This returns from getFilmTitles()
+
+}
+
+
 module.exports = Cinema;
